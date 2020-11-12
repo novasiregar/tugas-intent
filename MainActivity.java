689ba5ffe64bf3a09c03557752d2b38ad59d14cd
novasiregar <72541112+novasiregar@users.noreply.github.com>
2020-11-12 15:43:27 +0700

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,12 @@
+Button btnMoveActivity;
+
+@Override
+
+protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_main);
+  
+  
+  btnMoveActivity = findViewById(R.id.btn_move_activity);
+  
+  }
